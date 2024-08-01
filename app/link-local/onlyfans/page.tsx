26fb6e075495b797b6a24f3e153ca001b79b8890
Silas Schulreich <silas.schulreich@gmail.com>
2024-08-01 15:27:04 +0200

    FEATURE: add back link to local link

--- app/link-local/onlyfans/page.tsx
+++ app/link-local/onlyfans/page.tsx
@@ -1,5 +1,6 @@
 "use server";
 
+import BackLinkComponent from "@/components/back-link.component";
 import PointingLeonardoGif from "@/public/gifs/pointing_leonardo.gif";
 import Image from "next/image";
 
@@ -7,19 +8,20 @@ import Image from "next/image";
 export default async function OnlyfansPage() {
 	return (
 		<main className={"flex flex-col justify-center"}>
-			<section className={"w-full h-fit py-1 border-t border-black flex flex-col items-center overflow-hidden"}>
+			<section className={"w-full h-fit py-1 border-t border-black flex flex-col items-center overflow-hidden " +
+				"-z-10"}>
 				<span className={"w-fit text-3xl text-black text-center font-merriweather font-bold uppercase " +
 					"select-none -translate-y-full animate-movein-from-bottom"}
 					style={{animationDelay: "400ms"}}>
 					you naughty piece
 				</span>
 			</section>
-			<section className={"w-full h-fit p-8 border-y border-black flex flex-col items-center relative"}>
+			<section className={"w-full h-fit p-8 border-y border-black flex flex-col items-center relative -z-10"}>
 				<Image className={"shadow-2d border-2 border-black rounded-md brightness-200"}
 					src={PointingLeonardoGif as unknown as string}
 					alt={""} />
 			</section>
-			<section className={"w-full h-fit py-1 border-b border-black overflow-hidden"}>
+			<section className={"w-full h-fit py-1 border-b border-black overflow-hidden -z-10"}>
 				<div className={"w-full h-fit text-black text-center font-merriweather font-bold uppercase " +
 					"select-none flex flex-col items-center translate-y-full animate-movein-from-top"}
 					style={{animationDelay: "1200ms"}}>
@@ -31,6 +33,7 @@ export default async function OnlyfansPage() {
 				</span>
 				</div>
 			</section>
+			<BackLinkComponent />
 		</main>
 	);
 }
