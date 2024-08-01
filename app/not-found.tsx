a9dade482eae81359c802f2747eb2132949e8567
Silas Schulreich <silas.schulreich@gmail.com>
2024-08-01 11:54:12 +0200

    FEATURE: add 404 page

---
+++ app/not-found.tsx
@@ -0,0 +1,14 @@
+"use server";
+
+export default async function NotFound() {
+	return (
+		<main className={"flex flex-col justify-center"}>
+			<section className={"border-y border-black flex flex-col items-center relative after:w-full " +
+				"after:h-full after:bg-white after:absolute after:-z-10 after:translate-y-1 after:translate-x-1"}>
+				<span className={"text-8xl font-roboto-slab select-none"}>
+					404
+				</span>
+			</section>
+		</main>
+	);
+}
