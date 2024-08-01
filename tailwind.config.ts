ca2c69eb00e1a3426d92d498092e0dde3a261b6f
Silas Schulreich <silas.schulreich@gmail.com>
2024-08-01 10:05:59 +0200

    FEATURE: extend tailwind theme with boxShadow and animation styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,18 @@ const config: Config = {
 				"roboto-slab": ["var(--font-roboto-slab)"],
 				"merriweather": ["var(--font-merriweather)"],
 			},
+			boxShadow: {
+				"2d": "4px 4px 0px 0px rgb(0,0,0)",
+			},
+			keyframes: {
+				"movein-from-bottom": {
+					"0%": {transform: "translateY(200%)"},
+					"50%, 100%": {transform: "translateY(0%)"},
+				},
+			},
+			animation: {
+				"movein-from-bottom": "movein-from-bottom 1600ms ease-out forwards",
+			},
 		},
 	},
 	plugins: [],
