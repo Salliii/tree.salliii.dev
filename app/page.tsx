6ee627c6da41eef58da56eb8b96bbf7b79e1e6d7
Silas Schulreich <silas.schulreich@gmail.com>
2024-09-03 09:17:37 +0200

    FEATURE: add database integration and remove static config

--- app/page.tsx
+++ app/page.tsx
@@ -2,46 +2,15 @@
 
 import FooterComponent from "@/components/footer.component";
 import LinkComponent from "@/components/link.component";
-import GithubIcon from "@/public/svgs/links/github.svg";
-import InstagramIcon from "@/public/svgs/links/instagram.svg";
-import LinkedinIcon from "@/public/svgs/links/linkedin.svg";
-import PinterestIcon from "@/public/svgs/links/pinterest.svg";
-import SnapchatIcon from "@/public/svgs/links/snapchat.svg";
-import SpotifyIcon from "@/public/svgs/links/spotify.svg";
-
-
-const staticLinkConfig: {title: string, highlighted: boolean, newTab: boolean, href: string, icon: any}[] = [
-	{
-		title: "Instagram", highlighted: false, newTab: true,
-		href: "https://www.instagram.com/salliii_02", icon: InstagramIcon,
-	},
-	{
-		title: "Pinterest", highlighted: false, newTab: true,
-		href: "https://pin.it/5FH6yjRRS", icon: PinterestIcon,
-	},
-	{
-		title: "GitHub", highlighted: false, newTab: true,
-		href: "https://github.com/Salliii", icon: GithubIcon,
-	},
-	{
-		title: "LinkedIn", highlighted: false, newTab: true,
-		href: "https://www.linkedin.com/in/silas-schulreich", icon: LinkedinIcon,
-	},
-	{
-		title: "Snapchat", highlighted: false, newTab: true,
-		href: "https://www.snapchat.com/add/salliii02?share_id=eo-y931-_lk", icon: SnapchatIcon,
-	},
-	{
-		title: "Spotify", highlighted: false, newTab: true,
-		href: "https://open.spotify.com/user/5otfy4jykovu1ypl938xzn7zf?si=Yxn1R807Teu2kBcuI-upnA", icon: SpotifyIcon,
-	},
-	{
-		title: "🌶️ Spicy Content 🍑🍆", highlighted: true, newTab: false,
-		href: `${process.env.BASE_URL}/link-local/onlyfans`, icon: undefined,
-	},
-];
+import {prismaClient} from "@/prisma/client";
 
 export default async function RootPage() {
+	const links = await prismaClient.link.findMany({
+		where: {visible: true},
+		orderBy: {index: "asc"},
+		include: {svg: true},
+	});
+
 	return (
 		<main className={"flex flex-col"}>
 			<section className={"w-full h-fit border-b border-black flex flex-col items-center overflow-hidden"}>
@@ -52,13 +21,13 @@ export default async function RootPage() {
 				</span>
 			</section>
 			<ul className={"w-full h-fit p-8 flex flex-col gap-4"}>
-				{staticLinkConfig.map((link, index) => {
-					return <LinkComponent key={index}
+				{links.map((link) => {
+					return <LinkComponent key={link.index}
 						title={link.title}
 						highlighted={link.highlighted}
-						newTab={link.newTab}
+						newTab={!link.href.includes("link-local")}
 						href={link.href}
-						Icon={link.icon} />;
+						svg={link.svg?.svg || ""} />;
 				})}
 			</ul>
 			<FooterComponent />
