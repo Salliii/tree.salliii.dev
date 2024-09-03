fae5e368d2a4bf9f6bf3d55b01c859d78d0bb009
Silas Schulreich <silas.schulreich@gmail.com>
2024-09-03 11:48:19 +0200

    REFACTOR: add fonts directly to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,23 @@
 "use server";
 
-import {fontMerriweather, fontRobotoSlab} from "@/public/fonts";
+import {Merriweather, Roboto_Slab} from "next/font/google";
 import React from "react";
 import "@/public/styles/globals.css";
+
+
+const fontRobotoSlab = Roboto_Slab({
+	display: "swap",
+	subsets: ["latin"],
+	weight: ["400", "700"],
+	variable: "--font-roboto-slab",
+});
+
+const fontMerriweather = Merriweather({
+	display: "swap",
+	subsets: ["latin"],
+	weight: ["400", "700"],
+	variable: "--font-merriweather",
+});
 
 
 export default async function RootLayout({
