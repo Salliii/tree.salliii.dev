3eef8d99c7194731695742043d2f75dbba03f5b7
Silas Schulreich <silas.schulreich@gmail.com>
2024-09-03 13:27:26 +0200

    FEATURE: add svg service and update svg api routes

--- app/api/svg/route.ts
+++ app/api/svg/route.ts
@@ -1,97 +1,162 @@
-import {prismaClient} from "@/prisma/client";
+import SvgService from "@/lib/services/svg.service";
 import svgoConfig from "@/svgo.config.mjs";
 import {NextRequest, NextResponse} from "next/server";
 import {optimize} from "svgo";
+import {z} from "zod";
 
 
 export const dynamic = "force-dynamic";
 
 export async function GET() {
-	const result = await prismaClient.svg.findMany();
-
-	return NextResponse.json(result, {status: 200});
+	return SvgService
+		.getAllSvgs()
+		.then((result) => {
+			return NextResponse.json(
+				result,
+				{status: 200},
+			);
+		})
+		.catch(() => {
+			return NextResponse.json(
+				"500 Internal Server Error",
+				{status: 500},
+			);
+		});
 }
 
 
 export async function POST(request: NextRequest) {
 	const secret = request.nextUrl.searchParams.get("secret");
 	if (!secret || secret != process.env.SECRET) {
-		return NextResponse.json("401 Unauthorized", {status: 401});
+		return NextResponse.json(
+			"401 Unauthorized",
+			{status: 401},
+		);
 	}
 
-	const {name, svg}: {
-		name: string,
-		svg: string,
-	} = await request.json();
+	const body = z.object({
+		name: z.string(),
+		svg: z.string(),
+	}).safeParse(await request.json());
 
-	let optimizedSvg = undefined;
-	try {
-		optimizedSvg = optimize(svg, svgoConfig as any).data;
-	} catch (error) {
-		return NextResponse.json("424 Failed Dependency: svgo optimization", {status: 424});
+
+	if (body.success) {
+		try {
+			body.data.svg = optimize(body.data.svg, svgoConfig as any).data;
+		} catch (error) {
+			return NextResponse.json(
+				"424 Failed Dependency",
+				{status: 424},
+			);
+		}
+
+		return SvgService
+			.addSvg(body.data)
+			.then((result) => {
+				return NextResponse.json(
+					result,
+					{status: 200},
+				);
+			})
+			.catch(() => {
+				return NextResponse.json(
+					"400 Bad Request",
+					{status: 400},
+				);
+			});
 	}
 
-
-	const result = await prismaClient.svg.create({
-		data: {
-			name,
-			svg: optimizedSvg,
-		},
-	});
-
-	return NextResponse.json(result, {status: 201});
+	return NextResponse.json(
+		"400 Bad Request",
+		{status: 400},
+	);
 }
 
 export async function PATCH(request: NextRequest) {
 	const secret = request.nextUrl.searchParams.get("secret");
 	if (!secret || secret != process.env.SECRET) {
-		return NextResponse.json("401 Unauthorized", {status: 401});
+		return NextResponse.json(
+			"401 Unauthorized",
+			{status: 401},
+		);
 	}
 
 	const id = request.nextUrl.searchParams.get("id");
 	if (!id) {
-		return NextResponse.json("400 Bad Request", {status: 400});
+		return NextResponse.json(
+			"400 Bad Request",
+			{status: 400},
+		);
 	}
 
-	const {name, svg}: {
-		name?: string | undefined,
-		svg?: string | undefined,
-	} = await request.json();
+	const body = z.object({
+		name: z.string().optional(),
+		svg: z.string().optional(),
+	}).safeParse(await request.json());
 
-	let optimizedSvg = undefined;
-	if (svg) {
-		try {
-			optimizedSvg = optimize(svg, svgoConfig as any).data;
-		} catch (error) {
-			return NextResponse.json("424 Failed Dependency: svgo optimization", {status: 424});
+	if (body.success) {
+		if (body.data.svg) {
+			try {
+				body.data.svg = optimize(body.data.svg, svgoConfig as any).data;
+			} catch (error) {
+				return NextResponse.json(
+					"424 Failed Dependency",
+					{status: 424},
+				);
+			}
 		}
+
+		return SvgService
+			.updateSvg(id, body.data)
+			.then((result) => {
+				return NextResponse.json(
+					result,
+					{status: 200},
+				);
+			})
+			.catch(() => {
+				return NextResponse.json(
+					"400 Bad Request",
+					{status: 400},
+				);
+			});
 	}
 
-	const result = await prismaClient.svg.update({
-		where: {id},
-		data: {
-			name,
-			svg: optimizedSvg,
-		},
-	});
-
-	return NextResponse.json(result, {status: 200});
+	return NextResponse.json(
+		"400 Bad Request",
+		{status: 400},
+	);
 }
 
 export async function DELETE(request: NextRequest) {
 	const secret = request.nextUrl.searchParams.get("secret");
 	if (!secret || secret != process.env.SECRET) {
-		return NextResponse.json("401 Unauthorized", {status: 401});
+		return NextResponse.json(
+			"401 Unauthorized",
+			{status: 401},
+		);
 	}
 
 	const id = request.nextUrl.searchParams.get("id");
 	if (!id) {
-		return NextResponse.json("400 Bad Request", {status: 400});
+		return NextResponse.json(
+			"400 Bad Request",
+			{status: 400},
+		);
 	}
 
-	const result = await prismaClient.svg.delete({
-		where: {id},
-	});
-
-	return NextResponse.json(result, {status: 200});
+	return SvgService
+		.deleteSvg(id)
+		.then((result) => {
+			return NextResponse.json(
+				result,
+				{status: 200},
+			);
+		})
+		.catch(() => {
+			return NextResponse.json(
+				"400 Bad Request",
+				{status: 400},
+			);
+		});
 }
